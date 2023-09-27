c64318d8cd2cd2e04d2b5a777721ca2fe1334ecd
Yoyo <yoyoc0421@gmail.com>
2023-09-27 15:21:55 +0800

    First Commit

---
+++ Dog/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+
+class Dogs(models.Model):
+	name = models.CharField(max_length=100)
+	image = models.ImageField(upload_to="media/")
+	bio = models.TextField()
+
+	def __str__(self):
+		return self.name
---
+++ Dog/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from .models import Dogs
+# Create your views here.
+def index(request):
+	return render(request, "dog/index.html")
+
+def dogs(request):
+	dogs = Dogs.objects.all()
+	return render(request, "dog/dogs.html", {
+		"dogs": dogs
+	})
+
+def cats(request):
+	return render(request, "dog/cats.html")
