eb9be7e915ef30374f9a0c6cd7372053c6ceeac0
Yoyo <yoyoc0421@gmail.com>
2023-11-13 19:47:08 -0800

    change url paths

--- Dog/urls.py
+++ Dog/urls.py
@@ -3,9 +3,9 @@ from . import views
 
 urlpatterns = [
 	path("", views.index, name="index"),
-	path("狗狗班", views.dogs, name="dogs"),
-	path("貓貓班", views.cats, name="cats"),
-    path("關於我們", views.about, name="about"),
-    path("支持我們的生活費", views.donation, name="donation"),
-    path("地址", views.volunteer, name="volunteer")
+	path("dogs", views.dogs, name="dogs"),
+	path("cats", views.cats, name="cats"),
+    path("about", views.about, name="about"),
+    path("donation", views.donation, name="donation"),
+    path("volunteer", views.volunteer, name="volunteer")
 ]
